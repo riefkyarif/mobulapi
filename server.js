61c33136bdb9d68077b9e88c372af921077855ca
riefkyarif <riefkyar.ed@protonmail.com>
2021-09-13 11:40:18 +0700

    debug code serverjs

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
+//var morgan = require('morgan');
+
 //parse application/json
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 //panggil routes
-var routes = require('./routes');
-routes(app);
+//var routes = require('./routes');
+//routes(app);
+
+//datarkan menu routes dari index
+//app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
