4ed7f18d330a1cf23f8cb774db60cc131301ee75
riefkyarif <riefkyar.ed@protonmail.com>
2021-09-13 12:19:59 +0700

    successed showing all sql data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./connection');
 exports.index = function(req, res){
     response.ok("App RestAPI worked", res);
 }
+
+// menampilkan semua data pengguna
+exports.tampilsemuapengguna = function(req,res){
+    connection.query('SELECT * FROM pengguna', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
+};
